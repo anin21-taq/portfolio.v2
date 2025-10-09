c1bc30ec58988f8928e38a558bd74c25febaa500
anin21-taq <anindrataqi1@gmail.com>
2025-10-09 17:13:29 +0000

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,40 +1,39 @@
 const path = require("path")
 
-// Create pages dynamically if needed
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   
-  // Example: Create project detail pages
   const result = await graphql(`
-  query {
-    allDataJson {
-      edges {
-        node {
-          projects {
-            title
-            slug
+    query {
+      allDataJson {
+        edges {
+          node {
+            projects {
+              title
+              slug
+            }
           }
         }
       }
     }
+  `)
+
+  if (result.errors) {
+    throw result.errors
   }
-`)
 
-if (result.errors) {
-  throw result.errors
-}
+  result.data.allDataJson.edges.forEach(({ node }) => {
+    node.projects.forEach(project => {
+      const safeSlug = project.slug || project.title.toLowerCase().replace(/\s+/g, "-")
 
-result.data.allDataJson.edges.forEach(({ node }) => {
-  node.projects.forEach(project => {
-    actions.createPage({
-      path: `/project/${project.slug}`,
-      component: path.resolve("./src/templates/project-detail.js"),
-      context: {
-        title: project.title,
-        slug: project.slug
-      },
+      createPage({
+        path: `/project/${safeSlug}`,
+        component: path.resolve("./src/templates/project-detail.js"),
+        context: {
+          title: project.title,
+          slug: safeSlug,
+        },
+      })
     })
   })
-})
-
 }
